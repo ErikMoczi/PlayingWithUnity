f5272e7ea958513349530b180f9df84fa71f0f8b
Erik Móczi <32465761+ErikMoczi@users.noreply.github.com>
2018-01-25 17:04:14 +0100

    Add chapter - 1.2. Building a Graph

---
+++ Assets/1_Basics/02_BuildingAGraph/Graph.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    public Transform PointPrefab;
+    [Range(10, 100)] public int Resolution = 10;
+    private Transform[] _points;
+
+    private void Awake()
+    {
+        var step = 2f / Resolution;
+        var scale = Vector3.one * step;
+        var position = Vector3.one;
+        _points = new Transform[Resolution];
+
+        for (var i = 0; i < Resolution; i++)
+        {
+            var point = Instantiate(PointPrefab);
+            _points[i] = point;
+            position.x = (i + 0.5f) * step - 1f;
+            point.localPosition = position;
+            point.localScale = scale;
+            point.SetParent(transform, false);
+        }
+    }
+
+    private void Update()
+    {
+        foreach (var point in _points)
+        {
+            var position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time));
+            point.localPosition = position;
+        }
+    }
+}

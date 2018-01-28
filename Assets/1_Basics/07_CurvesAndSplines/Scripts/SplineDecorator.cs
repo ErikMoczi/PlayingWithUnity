c3ae6f16d4333f9028aa580535c71a7874b21fa3
Erik Móczi <32465761+ErikMoczi@users.noreply.github.com>
2018-01-28 16:08:54 +0100

    Add chapter - 1.7. Curves and Splines

---
+++ Assets/1_Basics/07_CurvesAndSplines/Scripts/SplineDecorator.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class SplineDecorator : MonoBehaviour
+{
+    public int frequency;
+
+    public Transform[] items;
+
+    public bool lookForward;
+    public BezierSpline spline;
+
+    private void Awake()
+    {
+        if (frequency <= 0 || items == null || items.Length == 0) return;
+        float stepSize = frequency * items.Length;
+        if (spline.Loop || stepSize == 1f)
+        {
+            stepSize = 1f / stepSize;
+        }
+        else
+        {
+            stepSize = 1f / (stepSize - 1);
+        }
+
+        for (int p = 0, f = 0; f < frequency; f++)
+        for (var i = 0; i < items.Length; i++, p++)
+        {
+            var item = Instantiate(items[i]);
+            var position = spline.GetPoint(p * stepSize);
+            item.transform.localPosition = position;
+            if (lookForward) item.transform.LookAt(position + spline.GetDirection(p * stepSize));
+
+            item.transform.parent = transform;
+        }
+    }
+}
